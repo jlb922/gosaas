ae78644085db37c0704e7d3af76912c41a921660
Jeff Bradbury <jlb922@gmail.com>
2020-05-21 05:39:37 -0400

    changes to gziphanfler

--- gzip.go
+++ gzip.go
@@ -1,59 +1,16 @@
 package gosaas
 
 import (
+	"fmt"
 	"net/http"
-	"strings"
-	"github.com./nytimes/gziphandler"
+
+	"github.com/NYTimes/gziphandler"
 )
 
 // Gzip is a middleware that compresses the responses
 func Gzip(next http.Handler) http.Handler {
-	fmt.println("In Gzip")
-	return gziphandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r.WithContext(ctx))
-	}))
-	}
-
-	/*return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := context.WithValue(r.Context(), ContextRequestStart, time.Now())
-		ctx = context.WithValue(ctx, ContextRequestID, uuid.NewV4().String())
-
+	fmt.Println("in gzip")
+	return gziphandler.GzipHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		next.ServeHTTP(w, r.WithContext(ctx))
-	}) */
+	}))
 }
-
-/* From GO Web Programming Pluralsight
-
-type GzipMiddleware struct {
-	Next http.Handler
-}
-
-func (gm *GzipMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if gm.Next == nil {
-		gm.Next = http.DefaultServeMux
-	}
-
-	encodings := r.Header.Get("Accept-Encoding")
-	if !strings.Contains(encodings, "gzip") {
-		gm.Next.ServeHTTP(w, r)
-		return
-	}
-	w.Header().Add("Content-Encoding", "gzip")
-	gzipwriter := gzip.NewWriter(w)
-	defer gzipwriter.Close()
-	grw := gzipResponseWriter{
-		ResponseWriter: w,
-		Writer:         gzipwriter,
-	}
-	gm.Next.ServeHTTP(grw, r)
-}
-
-type gzipResponseWriter struct {
-	http.ResponseWriter
-	io.Writer
-}
-
-func (grw gzipResponseWriter) Write(data []byte) (int, error) {
-	return grw.Writer.Write(data)
-}
-*/
